1b1aee54bf662d3712555460c5b34f68ed0d1175
VivekTakarde <120901714+VivekTakarde@users.noreply.github.com>
2024-11-28 13:05:23 +0530

    Basic Commit
    Hi all, I have created project please go ahead and cone it and complete your task....

---
+++ SampleProject/src/pageLayer/TestBase.java
@@ -0,0 +1,12 @@
+package pageLayer;
+
+public class TestBase {
+
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World...");
+		
+		System.out.println("TestBase");
+		System.out.println("Added Dependencies in pom.xml");
+	}
+}
